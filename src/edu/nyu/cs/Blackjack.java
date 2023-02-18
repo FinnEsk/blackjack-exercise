47bf99e19bc45efe091b25c78467f8427d7ed42b
FinnEsk <fee9992@nyu.edu>
2023-02-17 21:27:00 -0500

    1.1

--- src/edu/nyu/cs/Blackjack.java
+++ src/edu/nyu/cs/Blackjack.java
@@ -1,6 +1,7 @@
 package edu.nyu.cs;
 
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A variation of the game of Blackjack.  
@@ -13,9 +14,34 @@ public class Blackjack {
    * 
    * @param args An array of any command-line arguments.
    */
+
+  public static int generate_card(){
+    Random random_class = new Random();
+    int card = random_class.nextInt(9) + 2;
+    return card;
+  }
+
+  public static boolean hit_stand(){
+    Scanner scnr = new Scanner(System.in);
+    String choice = scnr.nextLine();
+    System.out.println("Would you like to hit or stand? ");
+    
+  }
   public static void main(String[] args) throws Exception {
 
     // complete this function according to the instructions
+
+    // random class
+
+
+
+    System.out.println("Welcome to Blackjack!");
+    int player_card1 = generate_card();
+    int player_card2 = generate_card();
+    
+
+
+
 
   } // main
 
