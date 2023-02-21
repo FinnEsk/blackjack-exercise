47493a279479952e8d90ee8d253d5c398be28739
FinnEsk <fee9992@nyu.edu>
2023-02-21 09:06:09 -0500

    1.3

--- src/edu/nyu/cs/Blackjack.java
+++ src/edu/nyu/cs/Blackjack.java
@@ -2,7 +2,8 @@ package edu.nyu.cs;
 
 import java.util.Scanner;
 import java.util.Random;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  * A variation of the game of Blackjack.  
  * Complete this program according to the instructions in the README.md file as well as within the given comments below.
@@ -36,6 +37,8 @@ public class Blackjack {
       return false;
     }
 
+  
+
     
   }
   public static void main(String[] args) throws Exception {
@@ -49,24 +52,42 @@ public class Blackjack {
     int player_card1 = generate_card();
     int player_card2 = generate_card();
     int player_total = player_card1 + player_card2;
-    boolean bust = false;
 
     System.out.println("Your cards are: " + player_card1 + "and " + player_card2);
 
-      
-      while (bust == false){
-        
+
+// card sentences
+
+ArrayList<Integer> player_cards = new ArrayList<Integer>();
+String player_sentence = "";
+
+
+
+for (int i = 0; i < player_cards.length; i++){
+    player_sentence += player_cards[i];
+    player_sentence += ", ";
+}
+
+
+      // player choice loop (this is where they decide whether to hit or stand)
+      while (true){
+        //if player busts
+        if (player_total > 21){
+          System.out.println("You have bust!\nDealer wins!");
+          break;
+        }
+        // 
         if (hit()){
           int new_card = generate_card();
           player_total += new_card;
+          System.out.println("Your cards are: " + player_card1 + ", " + player_card2 + );
         }
         else{
           break;
         }
-        if (player_total > 21){
-          bust = true;
-        }
       }
+
+
 
     
     
